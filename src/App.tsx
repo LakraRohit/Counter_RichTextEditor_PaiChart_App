6c41eb66205b74ec71d30624fa7387b28b14174c
ROHIT KUMAR LAKRA <145868503+LakraRohit@users.noreply.github.com>
2025-02-05 21:13:12 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from './redux/store'; // Import persistor
+import { persistor, store } from './redux/store'; 
 
 import Counter from './components/Counter';
 import UserForm from './components/UserForm';
